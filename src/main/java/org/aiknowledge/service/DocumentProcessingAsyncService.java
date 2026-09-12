b9dbcc1bc124b285594cc3c4f9adf37eb05d0a6e
ma3llim <mohdsameer68257@gmail.com>
2026-09-12 13:05:27 +0530

    feat: added the document processing

--- src/main/java/org/aiknowledge/service/DocumentProcessingAsyncService.java
+++ src/main/java/org/aiknowledge/service/DocumentProcessingAsyncService.java
@@ -3,11 +3,13 @@ package org.aiknowledge.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aiknowledge.entity.Document;
+import org.aiknowledge.processing.DocumentReaderFactory;
 import org.aiknowledge.repository.DocumentRepository;
 import org.springframework.core.io.Resource;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -16,16 +18,23 @@ import java.util.UUID;
 public class DocumentProcessingAsyncService {
     private final DocumentRepository documentRepository;
     private final ObjectStorageService objectStorageService;
+    private final DocumentReaderFactory documentReaderFactory;
 
     @Async("documentProcessingExecutor")
     public void processAsync(UUID documentId) {
         Document document = documentRepository.findById(documentId).orElseThrow(() ->
-                new IllegalArgumentException("Document not found: " + documentId)
-        );
-        String objectKey = document.getR2ObjectKey();
+                new IllegalArgumentException("Document not found: " + documentId));
 
+        String objectKey = document.getR2ObjectKey();
         Resource data = objectStorageService.loadFromR2(objectKey);
 
-        log.info("data: {}", data);
+        List<org.springframework.ai.document.Document> documents = documentReaderFactory.read(document.getFileType(), data);
+
+        log.info(
+                "Document extracted successfully. documentId={}, type={}, documents={}",
+                documentId,
+                document.getFileType(),
+                documents.size()
+        );
     }
 }
